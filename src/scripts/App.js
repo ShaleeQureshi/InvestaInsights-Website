a4a76562fcf1e301c7a6158efeb07074a3d20204
ShaleeQureshi <shaleequreshi2019@gmail.com>
2023-07-16 21:16:21 -0400

    Changes to be committed:

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import { Login_Page, Member_Resources_Pages } from "../views";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/login" element={<Login_Page />} />
         <Route path="/member-resources" element={<Member_Resources_Pages />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
